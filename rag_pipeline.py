072b4a2c20a275dc2997dcc09dff2d7379a9a0ee
VaidehiHattikar <vaidehihattikar@gmail.com>
2026-03-19 22:54:26 +0530

    Update rag_pipeline.py

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -2,26 +2,21 @@ import faiss
 import numpy as np
 from sentence_transformers import SentenceTransformer
 from google import genai
+import os
 
-# Initialize client
-client = genai.Client(api_key="AIzaSyBreO3TNW6vhcwlUUTAUNr74F38XgD-1tY")
-# Load embedding model
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 embed_model = SentenceTransformer('all-MiniLM-L6-v2')
 
-# Load FAISS index
 index = faiss.read_index("faiss_index.index")
 
-# Load chunks
 chunks = np.load("chunks.npy", allow_pickle=True)
 
 
-# Retrieve relevant chunks
 def retrieve(query, top_k=3):
     query_embedding = embed_model.encode([query]).astype("float32")
     distances, indices = index.search(np.array(query_embedding), top_k)
     return [chunks[i] for i in indices[0]]
 
-# Generate answer
 def generate_answer(query):
     context = retrieve(query)
     context_text = "\n\n".join(context)
@@ -50,7 +45,7 @@ Answer clearly:
     )
 
     return response.text
-# Run loop
+    
 while True:
     query = input("\nAsk your question (or type 'exit'): ")
     
